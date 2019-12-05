cd1f47fdd8843bc1b3c789921e79330391f02074
Robbie Porter <rthomporter@gmail.com>
2019-12-04 18:27:36 -0600

    MVP + stretch

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import Chart from "./Chart";
 
-const Charts = ({ coinData }) => {
+const Charts = props => {
   return (
     <div className="charts">
-      {coinData.map(coin => (
-        <div className="chart__container" key={coin.name}>
-          <h2 className="coin__title">{coin.name}</h2>
-          <h4 className="coin__symbol">{coin.symbol}</h4>
-          <div className="coin__logo">
-            <img src={coin.image} height="40" alt={coin.name} />
+      {props.coinData
+        .filter(chart => chart.name.includes(props.search))
+        .map(coin => (
+          <div className="chart__container" key={coin.name}>
+            <h2 className="coin__title">{coin.name}</h2>
+            <h4 className="coin__symbol">{coin.symbol}</h4>
+            <h4 className="coin__symbol">
+              Current Price: ${coin.current_price}
+            </h4>
+            <div className="coin__logo">
+              <img src={coin.image} height="40" alt={coin.name} />
+            </div>
+            <Chart sparklineData={coin.sparkline_in_7d.price} />
           </div>
-          <Chart sparklineData={coin.sparkline_in_7d.price} />
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import useDarkMode from "./hooks/useDarkMode";
+import styled from "styled-components";
 
-const Navbar = () => {
+const TogDiv = styled.div`
+  display: flex;
+  width: 15%;
+  justify-content: space-around;
+`;
+
+const Navbar = props => {
   const [darkMode, setDarkMode] = useDarkMode(false);
   const toggleMode = e => {
     e.preventDefault();
@@ -9,13 +16,27 @@ const Navbar = () => {
   };
   return (
     <nav className="navbar">
-      <h1>Crypto Tracker</h1>
-      <div className="dark-mode__toggle">
-        <div
-          onClick={toggleMode}
-          className={darkMode ? "toggle toggled" : "toggle"}
-        />
-      </div>
+      <a
+        className={darkMode ? "dark-mode-link" : "light-mode-link"}
+        href="./index.js"
+      >
+        <h1>Crypto Tracker</h1>
+      </a>
+      <input
+        className={darkMode ? "dark-mode-input" : "light-mode-input"}
+        placeholder="Search"
+        onChange={props.handleChanges}
+        value={props.search}
+      />
+      <TogDiv>
+        <h4>{darkMode ? "Light Mode" : "Dark Mode"}</h4>
+        <div className="dark-mode__toggle">
+          <div
+            onClick={toggleMode}
+            className={darkMode ? "toggle toggled" : "toggle"}
+          />
+        </div>
+      </TogDiv>
     </nav>
   );
 };
